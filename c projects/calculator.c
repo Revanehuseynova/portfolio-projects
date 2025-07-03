2139ccadbf9b137af326da9543adc7d87b96898d
Revanehuseynova <hrevane292@gmail.com>
2025-07-03 22:20:08 +0400

    Add files via upload

--- c projects/calculator.c
+++ c projects/calculator.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main() {
+    double num1, num2, result;
+    char operator;
+    printf("Birinci ədədi daxil edin: ");
+    scanf("%lf", &num1);
+    printf("Əməliyyatı seçin (+, -, *, /): ");
+    scanf(" %c", &operator);
+    printf("İkinci ədədi daxil edin: ");
+    scanf("%lf", &num2);
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf\n", result);
+            } else {
+                printf("0-a bölmək olmaz!\n");
+            }
+            break;
+    }
+    return 0;
+}
